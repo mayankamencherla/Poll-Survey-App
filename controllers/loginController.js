c4b7ac1262b69825026cb4bb8334f3141fbc69d0
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-15 17:13:17 +0530

    Moved Polls App to a MVS architecture

---
+++ controllers/loginController.js
@@ -0,0 +1,9 @@
+// Login page of the app
+
+var mongodb = require('mongodb');
+
+module.exports.controller = (app) => {
+    app.get('/', (request, response) => {
+        response.render('index/signin.hbs');
+    });
+}
