2e3c2a424361ecac5c568afe77d6ddd638e40b6d
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-15 23:52:14 +0530

    Added a signin and sign up button

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,6 +4,17 @@ var mongodb = require('mongodb');
 
 module.exports.controller = (app) => {
     app.get('/', (request, response) => {
-        response.render('signin.hbs');
+        response.render('signin.hbs', {
+            welcomeUrl: '/welcome',
+            signupUrl: '/signup'
+        });
+    });
+
+    app.get('/welcome', (request, response) => {
+        response.send('Welcome');
+    });
+
+    app.get('/signup', (request, response) => {
+        response.send('Signing up');
     });
 }
